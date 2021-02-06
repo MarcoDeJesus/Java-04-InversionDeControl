904d763a33fc5129b530b76f7c467cf50ed68c4c
Marco De Jesus <marco_dejesus@live.com>
2021-02-06 12:00:57 -0600

    Creando la interfaz que se encarga del Servicio de Cobranza con un método ProcesarPago(). Esto para que las dependencias sean cargadas en FormaDePago en tiempo de Ejecucion, de acuerdo a la forma de pago ya sea Debito o Credito.

--- src/FormaDePago.java
+++ src/FormaDePago.java
@@ -1,6 +1,13 @@
 public class FormaDePago implements IServiciosFormaDePago{
+
+    private IServicioDeCobranza servicioDeCobranza;
+
+    public FormaDePago(IServicioDeCobranza servicioDePagoSeleccionado){
+        this.servicioDeCobranza = servicioDePagoSeleccionado;
+    }
+
     @Override
     public void RealizaElCargoALaFormaDePago() {
-
+        this.servicioDeCobranza.ProcesarPago();
     }
 }
---
+++ src/IServicioDeCobranza.java
@@ -0,0 +1,3 @@
+public interface IServicioDeCobranza {
+    void ProcesarPago();
+}

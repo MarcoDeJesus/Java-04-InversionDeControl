5281e3525d4739a9b909bc94475b23f9e4c341fd
Marco De Jesus <marco_dejesus@live.com>
2021-02-06 12:29:08 -0600

    Añandiendo un proceso de identificación del tipo de Pago- Tarjeta de Debito

--- src/FormaDePagoDeTarjetaDeDebito.java
+++ src/FormaDePagoDeTarjetaDeDebito.java
@@ -1,5 +1,9 @@
 public class FormaDePagoDeTarjetaDeDebito implements IFormasDePago{
 
+    public FormaDePagoDeTarjetaDeDebito(){
+        System.out.println("La Forma de Pago se ha identificado como: TARJETA DE DÉBITO");
+    }
+
     @Override
     public IServiciosFormaDePago obtenerFormaDePago() {
         return new FormaDePago(new CobranzaTarjetaDebito());

5a7be9a4397a6986f3b43856bb74d1665f81a59c
Marco De Jesus <marco_dejesus@live.com>
2021-02-06 12:27:25 -0600

    Utilizando el Servicio de Pago/Cobro para procesar un pago desde una Tarjeta de Debito

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String args[]){
 
+        IServiciosFormaDePago servicioDeCobro;
+
+        // Pagar con un a Tarjeta de Debito
+        servicioDeCobro = new FormaDePagoDeTarjetaDeDebito().obtenerFormaDePago();
+        servicioDeCobro.RealizaElCargoALaFormaDePago();
     }
 }

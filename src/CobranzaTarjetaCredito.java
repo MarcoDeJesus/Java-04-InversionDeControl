7a77e6c1f7f0bd5a021c57bad74eca8987014bfa
Marco De Jesus <marco_dejesus@live.com>
2021-02-06 12:41:26 -0600

    Actualizando el mensaje a Crédito

--- src/CobranzaTarjetaCredito.java
+++ src/CobranzaTarjetaCredito.java
@@ -2,6 +2,6 @@ public class CobranzaTarjetaCredito implements IServicioDeCobranza{
     @Override
     public void ProcesarPago() {
         System.out.println("Pagando con Tarjeta de Crédito. A 24 Meses sin intereses.");
-        System.out.println("El pago con su Tarjeta de Débito se ha realizado con Éxito!!!");
+        System.out.println("El pago con su Tarjeta de Crédito se ha realizado con Éxito!!!");
     }
 }

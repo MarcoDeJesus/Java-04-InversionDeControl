4e0cdebcc683d189e814fe58ffb271a9b1c03aed
Marco De Jesus <marco_dejesus@live.com>
2021-02-06 11:51:02 -0600

    Eliminación de clases Simples con Dependencia y creación de la Clase de FormaDepago

--- src/CobranzaTarjetaDebito.java
+++
@@ -1,6 +0,0 @@
-public class CobranzaTarjetaDebito {
-    public void ProcesarPago() {
-        System.out.println("Pagando con Tarjeta de Débito. Con un 30% de Descuento.");
-        System.out.println("El pago con su Tarjeta de Débito se ha realizado con Éxito!!!");
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String args[]){
-        CobranzaTarjetaDebito cobranza = new CobranzaTarjetaDebito();
-        cobranza.ProcesarPago();
+
     }
 }
